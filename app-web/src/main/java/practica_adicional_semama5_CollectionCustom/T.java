9d05550c4047ecf30ea9bb8901edd71707316e87
DiegoLentz <diegoolentz@gmail.com>
2022-06-02 15:07:22 -0300

    practica_adicional_semama5_CollectionCustom

---
+++ app-web/src/main/java/practica_adicional_semama5_CollectionCustom/T.java
@@ -0,0 +1,16 @@
+package practica_adicional_semama5_CollectionCustom;
+
+public class T {
+	private String nombre;
+
+	public T(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+	
+	
+
+}
